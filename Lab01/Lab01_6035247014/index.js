b0b1d6cba06718255a231c03499ee5ce7bdfcd30
somwatee <night5099@gmail.com>
2019-11-26 23:27:00 +0700

    Add Lab

---
+++ Lab01/Lab01_6035247014/index.js
@@ -0,0 +1,25 @@
+console.log("Night");
+let firstName="winai";
+let lastName="authai";
+let age=5;
+let Y=0.1;
+let Area4=0;
+let Area3=0;
+let wide=0;
+let long=0;
+let high=0;
+let base=0;
+function squareNumber(Number)
+{
+    return Number*Number;
+}
+
+let result1=squareNumber(5);
+
+function squareArea(wide,long,high,base)
+{
+    Area4=wide*long;
+    Area3=0.5*high*base;
+    return Area4+Area3;
+}
+//let resultArea=squareArea();
